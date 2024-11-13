46b16246ca9c4f80ce9a3eff30ba9b068759f357
Akerman048 <akkerman048@gmail.com>
2024-11-13 22:48:30 +0000

    Initial commit

--- src/pages/Error/Error.tsx
+++ src/pages/Error/Error.tsx
@@ -1,12 +1,16 @@
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
 import s from "./Error.module.css";
+import { PiSmileySad } from "react-icons/pi";
 
 const Error: FC = () => {
   return (
     <div className={s.error}>
-      404 Not Found
-      <Link to='/'>Home</Link>
+      <PiSmileySad className={s.smile} />
+      <span>404</span> <h3>Page not found</h3>
+      <Link to='/'>
+        <button className={s.button}>Back to home</button>
+      </Link>
     </div>
   );
 };
