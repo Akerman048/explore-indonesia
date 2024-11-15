e6cfabf2470586c1b62dbb41dd0704073bbe722b
Akerman048 <akkerman048@gmail.com>
2024-11-15 17:45:50 +0000

    Initial commit

--- src/pages/RootLayout.tsx
+++ src/pages/RootLayout.tsx
@@ -7,22 +7,49 @@ import s from "./RootLayout.module.css";
 import Burger from "../components/Burger/Burger";
 import Footer from "../components/Footer/Footer";
 import logoName from "../images/logo/logoName.png";
-import logoPicture from "../images/logo/logoPicture.png"
-
-
+import logoPicture from "../images/logo/logoPicture.png";
+import { IoIosArrowDropupCircle } from "react-icons/io";
 
 const RootLayout: FC = () => {
   const [burger, setBurger] = useState<boolean>(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
 
   const burgerToggle = (): void => {
     setBurger((prevBurger) => !prevBurger);
   };
 
+  const scrollUp = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   useEffect(() => {
     console.log(burger);
   }, [burger]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => {
+      window.removeEventListener("scroll", toggleVisibility);
+    };
+  }, []);
   return (
     <div className={s.container}>
+      {isVisible && (
+        <div className={s.arrowUp} onClick={scrollUp}>
+          <IoIosArrowDropupCircle className={s.arrowUp} />
+        </div>
+      )}
       <header className={s.wrapper}>
         <div className={s.logoNavWrap}>
           <div className={s.logo}>
